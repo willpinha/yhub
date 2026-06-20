a3e4a4ee765840917656dd91623ba64d41a762f8
Willian Pinheiro <willpinhaaa@gmail.com>
2026-06-20 17:48:14 -0300

    Add clone command implementation and integrate with CLI app

--- main.go
+++ main.go
@@ -7,10 +7,11 @@ import (
 
 	"github.com/spf13/afero"
 	yhubcli "github.com/willpinha/yhub/internal/cli"
+	gitpkg "github.com/willpinha/yhub/internal/git"
 )
 
 func main() {
-	app := yhubcli.NewApp(afero.NewOsFs())
+	app := yhubcli.NewApp(afero.NewOsFs(), gitpkg.New())
 	if err := app.Run(context.Background(), os.Args); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
